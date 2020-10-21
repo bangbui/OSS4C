6fd1adb05e7f570e389d0255833204f8ba43a218
Bang <buinhatbang@gmail.com>
2020-10-21 16:52:48 +0700

    hoan thanh dien tich

---
+++ Project1/Project1/Dientich.cpp
@@ -0,0 +1,14 @@
+#include "dientich.h"
+
+float dienTichHCN(float dai, float rong)
+{
+	return dai * rong;
+}
+float dienTichVuong(float canh)
+{
+	return canh * canh;
+}
+float dienTichTron(float banKinh)
+{
+	return banKinh * banKinh*3.14;
+}
--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "dientich.h"
 using namespace std;
 
 int main()
@@ -14,6 +15,35 @@ int main()
 	cout << "Tich " << a << " va " << " la " << a * b << endl;
 	cout << "Thuong " << a << " va " << " la " << a / b << endl;
 
+	int hinh;
+	cout << "Ban muon tinh dien tich hinh nao (1:HCN; 2:Vuong; 3: Tron)? ";
+	cin >> hinh;
+	switch (hinh)
+	{
+	case 1:
+		float dai, rong;
+		cout << "Nhap chieu dai: ";
+		cin >> dai;
+		cout << "Nhap chieu rong: ";
+		cin >> rong;
+		cout << "Dien tich HCN = " << dienTichHCN(dai,rong) << endl;
+		break;
+	case 2:
+		float canh;
+		cout << "Nhap canh: ";
+		cin >> canh;
+		cout << "Dien tich vuong = " << dienTichVuong(canh) << endl;
+		break;
+	case 3:
+		float banKinh;
+		cout << "Nhap ban kinh: ";
+		cin >> banKinh;
+		cout << "Dien tich vuong = " << dienTichTron(banKinh) << endl;
+		break;
+	default:
+		break;
+	}
+	float dai, rong;
 	system("pause");
 	return 0;
 }
