dbbde38f653e2c11bdd4f1b18f00b682044ba285
Bang <buinhatbang@gmail.com>
2020-10-21 15:57:28 +0700

    them chuc nang +,-,*,/

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	int a, b;
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "Nhap b: ";
+	cin >> b;
+
+	cout << "Tong " << a << " va " << " la " << a + b << endl;
+	cout << "Hieu " << a << " va " << " la " << a - b << endl;
+	cout << "Tich " << a << " va " << " la " << a * b << endl;
+	cout << "Thuong " << a << " va " << " la " << a / b << endl;
+
 	system("pause");
 	return 0;
 }
